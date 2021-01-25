82c0f63feddb8099c5df6f8cf17f154de8e3b7d1
THULASIPRABHA <69714505+THULASIPRABHA@users.noreply.github.com>
2021-01-25 14:08:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,23 @@ import posttoot
 import tweet
 from datetime import date
 from PIL import Image
+from resizeimage import resizeimage
 #extract pic of the day
 imgdata=potd.fetch_potd(cur_date=date.today())
 imgsrc=imgdata['image_src']
 pageurl=imgdata['image_page_url']
 
 potd.saveimg(imgsrc)
+
+
+#fileconvertion
+fd_img = open("picoftheday.jpg", 'rb')
+img = Image.open(fd_img)    
+img = resizeimage.resize_contain(img, [1280, 1280])
+img.save("resize.png", img.format)
+fd_img.close()
 #toot
-posttoot.post("picoftheday.jpg",imgsrc,pageurl)
+posttoot.post("resize.png",imgsrc,pageurl)
 print("Sucessfully posted your toot")
-#tweet
-img = Image.open("picoftheday.jpg")       
-w=img.width
-h=img.height
-if w>=800 and h>=500:
-    new_wid = 500
-    new_heig=300
-    img1 = img.resize((new_wid,new_heig),Image.ANTIALIAS).show()
-img.save("resize.jpg")
-tweet.post_tweet("resize.jpg",imgsrc,pageurl)
+tweet.post_tweet("resize.png",imgsrc,pageurl)
 print("Sucessfully posted your tweet")
